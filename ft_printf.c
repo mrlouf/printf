ed0965e3789013c44913cbdfa76dffa5394859a1
Nicolas Ponchon <nponchon@car13s2.42barcelona.com>
2024-10-01 17:38:03 +0200

    git init

--- ft_printf.c
+++ ft_printf.c
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nponchon <nponchon@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 13:24:36 by nponchon          #+#    #+#             */
-/*   Updated: 2024/10/01 13:26:21 by nponchon         ###   ########.fr       */
+/*   Created: 2024/10/01 14:57:51 by nponchon          #+#    #+#             */
+/*   Updated: 2024/10/01 17:08:20 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdarg.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...)
+{
+	va_list		args;
+	size_t		i;
+	size_t		count;
+
+	va_start(args)
+	i = 0;
+	while (i < ft_strlen(format))
+	{
+		if ()
+		{
+			
+		}
+		write(1, format[i], 1);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ ft_printf.h
@@ -0,0 +1,13 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+//	LIBRARIES
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+//	FUNCTIONS
+
+#endif
